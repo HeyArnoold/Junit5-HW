228a3dc85aa21e88c186b109c235a1dfe1fabd38
Antonov Maksim <maks.antonov.2012@gmail.com>
2025-10-17 12:49:17 +0300

    FindMaxChar

---
+++ src/main/java/com/example/FindMaxChar.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindMaxChar {
+
+    public static void main(String[] args) {
+        System.out.println(findMaxChar("abababaads"));
+    }
+
+    static String findMaxChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        String ans = "";
+        int ansCount = 0;
+        char[] chars = s.toCharArray();
+
+        for (char now : chars) {
+            if (!map.containsKey(now)) {
+                map.put(now, 0);
+            }
+            map.put(now, map.get(now) + 1);
+        }
+
+        for (Character c : map.keySet()) {
+            if (map.get(c) > ansCount) {
+               ansCount = map.get(c);
+               ans = c.toString();
+            }
+        }
+        return ans;
+    }
+}
